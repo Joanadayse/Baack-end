f15e3963662ab77f353aa3c3753e8d81ef784fea
Joanadayse <daysejoana3@gmail.com>
2023-04-10 22:01:50 -0300

    intro-kenex

---
+++ introducao-knex-template/src/database/introducao-knex.sql
@@ -0,0 +1,16 @@
+-- Active: 1681167221003@@127.0.0.1@3306
+
+-- Tabelas já foram criadas
+CREATE TABLE bands (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE songs (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    name TEXT NOT NULL,
+    band_id TEXT NOT NULL,
+    FOREIGN KEY (band_id) REFERENCES bands (id)
+);
+
+SELECT*FROM bands;
